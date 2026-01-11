5b3a193eff66a5167a8c65184f1240d35a743ab2
Kushagra Chandra <kushagrachandra3@gmail.com>
2026-01-11 16:55:47 +0530

    Added Strikethrough Effect

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ export default function Card(props){
     return(
         <div className="item-text">
             <input type="checkbox" checked={props.done} onChange={()=>props.onToggle(props.id)}/>
-            <p>{props.text}</p>
+            <p style={{ textDecoration: props.done ? "line-through" : "none" }}>{props.text}</p>
             <button className="delete-btn" onClick={()=>props.remove(props.id)}><img src="../public/delete.png"></img></button>
         </div>
     )
